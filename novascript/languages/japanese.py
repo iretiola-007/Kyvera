9f3db9b9c02bd03945ea1b9ecb7948846680c852
Iretiola <abisigairetiola@gmail.com>
2026-02-23 18:26:34 +0100

    Create japanese.py

---
+++ novascript/languages/japanese.py
@@ -0,0 +1,23 @@
+KEYWORDS = { 
+    # Romaji
+    "hensuu": "VAR",
+    "shutsuryoku": "PRINT",
+    "moshi": "IF",
+    "kansu": "FUNCTION",
+    "modoru": "RETURN",
+    "moshi": "IF",
+    "soredehanai": "ELSE",
+
+    # Kanji
+    "変数": "VAR",
+    "出力": "PRINT",
+    "もし": "IF",
+    "関数": "FUNCTION",
+    "戻る": "RETURN",
+    "言語": "LANGUAGE",
+}
+
+ERRORS = {
+    "SYNTAX": "構文エラー",
+    "NAME": "名前エラー"
+}
