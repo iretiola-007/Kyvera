86449655d826936126985b71063450d60abf6ae1
Iretiola <abisigairetiola@gmail.com>
2026-02-23 18:26:05 +0100

    Create italian.py

---
+++ novascript/languages/italian.py
@@ -0,0 +1,15 @@
+KEYWORDS = {
+    "variabile": "VAR",
+    "stampa": "PRINT",
+    "se": "IF",
+    "funzione": "FUNC",
+    "ritorna": "RETURN",
+    "lingua": "LANGUAGE", 
+    "se": "IF",
+    "altrimenti": "ELSE",
+}
+
+ERRORS = {
+    "SYNTAX": "Errore di sintassi",
+    "NAME": "Errore di nome"
+}
