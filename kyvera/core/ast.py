8d306f516c8c5b2994593407574aa7753197dd9a
Fortune <fortunefoluso@gmail.com>
2026-06-18 12:55:33 +0100

    cleanup: add contributing.md, clean up merge conflicts, add __repr__ functions to ast nodes

--- kyvera/core/ast.py
+++ kyvera/core/ast.py
@@ -2,26 +2,66 @@ class ProgramNode:
     def __init__(self, statements):
         self.statements = statements
 
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the object
+        when printing to the terminal
+        """
+
+        return f"ProgramNode(statements={self.statements})"
+
 
 class VariableNode:
     def __init__(self, name, value):
         self.name = name
         self.value = value
 
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the object
+        when printing to the terminal
+        """
+
+        return f"VariableNode(name={self.name}, value={self.value})"
+
 
 class PrintNode:
     def __init__(self, expression):
         self.expression = expression
+
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the object
+        when printing to the terminal
+        """
+
+        return f"PrintNode(expression={self.expression})"
 
 
 class NumberNode:
     def __init__(self, value):
         self.value = value
 
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the object
+        when printing to the terminal
+        """
+
+        return f"NumberNode(value={self.value})"
+
 
 class StringNode:
     def __init__(self, value):
         self.value = value
+
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the object
+        when printing to the terminal
+        """
+
+        return f"StringNode(value={self.value})"
 
 
 class BinOpNode:
@@ -30,9 +70,26 @@ class BinOpNode:
         self.operator = operator
         self.right = right
 
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the object
+        when printing to the terminal
+        """
+
+        return f"BinOpNode(left={self.left}, operator='{self.operator}', right={self.right})"
+
+
 class VarAccessNode:
     def __init__(self, name):
         self.name = name
+
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the object
+        when printing to the terminal
+        """
+
+        return f"VarAccessNode(name={self.name})"
 
 
 class IfNode:
@@ -41,16 +98,24 @@ class IfNode:
         self.body = body
         self.else_body = else_body
 
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the object
+        when printing to the terminal
+        """
+
+        return f"IfNode(condition={self.condition}, body={self.body}, else_body={self.else_body})"
 
 
 class UnaryOpNode:
-    def __init(self, operator, node):
+    def __init__(self, operator, node):
         self.operator = operator
         self.node = node
 
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the object
+        when printing to the terminal
+        """
 
-class BinOpNode:
-    def __init__(self, left, op, right):
-        self.left = left
-        self.op = op
-        self.right = right
+        return f"UnaryOpNode(operator={self.operator}, node={self.node})"
